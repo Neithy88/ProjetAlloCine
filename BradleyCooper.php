3b43f491015154e3ed3c9df78e28f061bb779a88
TA-A <thecleaurelie.augustin@gmail.com>
2018-12-03 10:08:48 +0100

    essais n°2

--- BradleyCooper.php
+++ BradleyCooper.php
@@ -3,7 +3,7 @@
 //Le modèle contient toutes les fonctions d'appel à la base de données.
 
 
-$dbh = new PDO('mysql:host=localhost;dbname=allocine', 'root', '');
+$dbh = new PDO('mysql:host=localhost;dbname=allocine', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 function getOneMovie($id) {
     global $dbh;
